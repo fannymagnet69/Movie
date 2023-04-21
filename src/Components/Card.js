4f4a316108edd20c2bcbf5582ebc8e3a22292523
fannymagnet69 <tamojitsengupta1811@gmail.com>
2023-04-21 22:21:29 +0530

    first commit

---
+++ src/Components/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Card=(movie)=>{
+   
+    let img_path="https://image.tmdb.org/t/p/w500";
+    return(
+        <>
+            <div className="movie">
+                <img src={img_path+movie.info.poster_path} className="poster"></img>
+                <div className="movie-details">
+                    <div className="box">
+                        <h4 className="title">{movie.info.title}</h4>
+                        
+                    </div>
+                    <div className="overview">
+                        <h2>overview</h2>
+                        {movie.info.overview}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Card;
